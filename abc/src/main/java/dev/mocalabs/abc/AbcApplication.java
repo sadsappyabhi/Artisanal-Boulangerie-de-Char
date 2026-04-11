780990014a37a25ecc2fde2bb522cd970236a91c
Abhilasha <sadsappyabhi@proton.me>
2026-04-11 00:52:53 -0500

    Initial scaffolding of Spring Boot project

---
+++ abc/src/main/java/dev/mocalabs/abc/AbcApplication.java
@@ -0,0 +1,13 @@
+package dev.mocalabs.abc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbcApplication.class, args);
+	}
+
+}
